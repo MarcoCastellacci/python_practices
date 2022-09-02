91562fdad52f13fee8e4a2d83a5e225409a3db3c
Marco Castellacci <marco.casterllacci87@gmail.com>
2022-09-02 16:52:37 -0300

    area task

---
+++ task-area.py
@@ -0,0 +1,11 @@
+def triangle(base, hight):
+    area = base * hight / 2
+    print(area)
+    
+def circle(radio):
+    area = 3.14 * radio**2
+    print(area)
+    
+    
+triangle(22, 10)
+circle(4.5)

8fe498ff85ca68bb25bebacc7ab2a43023324f3d
Marco Castellacci <marco.castellacci87@gmail.com>
2022-09-03 12:45:59 -0300

    Task function completed

--- task-primeN.py
+++ task-primeN.py
@@ -5,7 +5,7 @@ def is_prime(n):
     for i in range(2, n):
         if (n % i) == 0:
             print(n, "No es Numero Primo")
-        break
+            return
     print(n, "Es un numero Primo")
 
 is_prime(number)
--- task-year.py
+++ task-year.py
@@ -2,4 +2,9 @@ año = input('Entre el año a verificar (4 digitos)')
 año = int(año)
 
 def año_bisiesto(year):
-    pass
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        print(year, "Es un año bisiesto")
+    else:
+        print(year, "No es año Bisiesto")
+        
+año_bisiesto(año)

c4f2a33c80c3e1ebccb91609a79efc8ad67ecad4
Marco Castellacci <marco.castellacci87@gmail.com>
2022-09-03 19:08:00 -0300

    "Module Task"

---
+++ main.py
@@ -0,0 +1,10 @@
+from calculator import suma,resta,multiplicar,divisor
+def main():
+    res = suma(2, 6)
+    res1 = resta(6,8)
+    res2 = multiplicar(2,8)
+    res3 = divisor(2,1)
+    print(res, res1, res2, res3)
+
+if __name__ == '__main__':
+    main()

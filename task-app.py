c898193a181895fb0adba12cf303c5734b2ac8a0
Marco Castellacci <marco.castellacci87@gmail.com>
2022-09-20 11:36:20 -0300

    Task app New

--- task-app.py
+++ task-app.py
@@ -1,4 +1,5 @@
 from distutils.cmd import Command
+from multiprocessing import Value
 import tkinter
 from tkinter import ttk
 from tkinter import Label
@@ -8,18 +9,27 @@ window = tkinter.Tk()
 window.title('Task Window')
 window.columnconfigure(0, weight=1)
 window.columnconfigure(1, weight=3)
+text_ = Label(window)
 
 def juego_num(resp):
-    num = random()
-    if num == resp:
-        text_.config(text= 'You are a Mentalist')
+    num = random.randint(0,100)
+    if num < resp:
+        text_.config(text= 'Your Number is Bigger')
+    elif num > resp:    
+        text_.config(text= 'Your Number is Smaller')
+    else:
+        text_.config(text= 'Your Number is Correct')    
+        
+num = tkinter.IntVar()
+num_obtenido = int(num.get())
+num_entry = ttk.Entry(textvariable=num)
+res_button = ttk.Button(window, text='Start Play', command=juego_num(num_obtenido))
 
-num_entry = ttk.Entry(Command=juego_num())
 
 num_entry.grid(row=0, column=0, padx=15, pady=15)
+res_button.grid(row=1, column=0, padx=5, pady=5)
 
 
-text_ = Label(window)
 text_.grid()
 
 window.mainloop()
